d3723a675340c0ce3f1819c87aae84ede9aa114b
Semen1455 <118077919+Semen1455@users.noreply.github.com>
2023-05-03 20:00:31 +0300

    Delete Yakymenko_Task_1.py

--- Yakymenko_Task_1.py
+++
@@ -1,8 +0,0 @@
-# Multiparadigmatic  programming languages:Task_1
-# Yakymenko Semen:IKM-221a
-
-print('Multiparadigmatic  programming languages:Task_1\nSemen Yakymenko:IKM-221a')
-
-print((('Vladislav' + ', ') * 44) + 'Vladislav')
-
-print((11.2/(11.2-211.3))+(1.2/(15.2+11.3)))
