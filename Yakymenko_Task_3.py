1cb4a592ed69c3acaaa300d0deab1682d8c17cb9
Semen1455 <semen.yakymenko@infiz.khpi.edu.ua>
2023-05-08 16:59:12 +0300

    task 3

--- Yakymenko_Task_3.py
+++ Yakymenko_Task_3.py
@@ -1,7 +1,6 @@
 # Multiparadigmatic  programming languages:Task_3
 # Yakymenko Semen:IKM-221a
 
-
 import math
 
 print('Multiparadigmatic  programming languages:Task_3\nSemen Yakymenko:IKM-221a')
@@ -9,8 +8,9 @@ print('Multiparadigmatic  programming languages:Task_3\nSemen Yakymenko:IKM-221a
 TEMPLATE = 'Please Enter {} : '
 x = int(input(TEMPLATE.format('x')))
 z = int(input(TEMPLATE.format('z')))
+
 if z != 2:
     res = (x * math.atan(z)) + math.exp((x + 3) / (z - 2))
-    print('Result is:', res)
+    print(f'Result is: {res}')
 else:
     print('Error,your function will not work,type something else')
