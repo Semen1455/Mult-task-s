ea964b89b9d2d138db8377ada12815c9ba92cfc4
Semen1455 <semen.yakymenko@infiz.khpi.edu.ua>
2023-05-08 17:18:42 +0300

    task 4

--- Yakymenko_Task_4.py
+++ Yakymenko_Task_4.py
@@ -3,14 +3,17 @@
 
 import math
 
-
 print('Multiparadigmatic  programming languages:Task_4\nYakymenko Semen:IKM-221a, Twenty-third(23) on the list')
 
-n = int(input('Input n: '))
-for i in range(1, math.factorial(n)):
-    if math.factorial(n) / i / (i+1) == i + 2:
-        print('Program is working', ':',  i, ' * ', i+1, ' * ',  i+2, ' = ', i * (i + 1) * (i + 2))
-        break
+try:
+    n = int(input('Enter n: '))
+    for i in range(1, math.factorial(n)):
+        if math.factorial(n) / i / (i + 1) == i + 2:
+            print('Program is working')
+            break
+        else:
+            print('There are no such numbers')
     else:
-        print('There are no such numbers')
-        break
+        print('Try to enter something else')
+except ValueError:
+    print('You wrote a letter,try to enter number')
