e2775af019831d4e6ab8a0bf9cedb9ce299332c4
Semen1455 <semen.yakymenko@infiz.khpi.edu.ua>
2023-05-08 16:48:38 +0300

    task 2

--- Yakymenko_Task_2.py
+++ Yakymenko_Task_2.py
@@ -1,11 +1,15 @@
 # Multiparadigmatic  programming languages:Task_2
 # Yakymenko Semen:IKM-221a
 
+
 print('Multiparadigmatic  programming languages:Task_2\nSemen Yakymenko:IKM-221a')
 TEMPLATE = 'Please Enter {} : '
-
-a = int(input(TEMPLATE.format('a')))
-b = int(input(TEMPLATE.format('b')))
-c = int(input(TEMPLATE.format('c')))
-res = ((11 + 2 * a + 4.1) / (12.4 - b)) + c
-print(res)
+TEMPLATE1 = 'You wrote a letter in {} ,try number'
+try:
+    a = int(input(TEMPLATE.format('a')))
+    b = int(input(TEMPLATE.format('b')))
+    c = int(input(TEMPLATE.format('c')))
+    res = ((11 + 2 * a + 4.1) / (12.4 - b)) + c
+    print(res)
+except ValueError:
+    print('You enter a letter,try to enter number')
