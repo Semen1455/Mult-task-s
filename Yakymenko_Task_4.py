ad9dc19d3499b30382838732e9253ef9e2bc30c6
Semen1455 <semen.yakymenko@infiz.khpi.edu.ua>
2023-05-09 19:05:29 +0300

    task 4

--- Yakymenko_Task_4.py
+++ Yakymenko_Task_4.py
@@ -1,19 +1,22 @@
-# Multiparadigmatic  programming languages:Task_4
-# Yakymenko Semen:IKM-221a
-
 import math
 
 print('Multiparadigmatic  programming languages:Task_4\nYakymenko Semen:IKM-221a, Twenty-third(23) on the list')
 
-try:
-    n = int(input('Enter n: '))
+
+found = False
+while not found:
+    def validate_inp():
+        while True:
+            try:
+                return int(input('Enter n: '))
+            except ValueError:
+                print('You wrote a letter,try to enter number')
+    n = validate_inp()
     for i in range(1, math.factorial(n)):
         if math.factorial(n) / i / (i + 1) == i + 2:
-            print('Program is working')
+            print(f'Program is working | {i} * {i + 1} * {i+2} = {(i * (i + 1) * (i + 2))}')
+            print(f'Job Check | {n}! is {math.factorial(n)} ')
+            found = True
             break
-        else:
-            print('There are no such numbers')
     else:
-        print('Try to enter something else')
-except ValueError:
-    print('You wrote a letter,try to enter number')
+        print('There are no such a numbers')
