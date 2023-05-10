80dd47330363335901b03c022333c3221a8de6a6
Semen1455 <semen.yakymenko@infiz.khpi.edu.ua>
2023-05-10 23:30:07 +0300

    task 4

--- Yakymenko_Task_4.py
+++ Yakymenko_Task_4.py
@@ -12,11 +12,17 @@ while not found:
             except ValueError:
                 print('You wrote a letter,try to enter number')
     n = validate_inp()
-    for i in range(1, math.factorial(n)):
-        if math.factorial(n) / i / (i + 1) == i + 2:
-            print(f'Program is working | {i} * {i + 1} * {i+2} = {(i * (i + 1) * (i + 2))}')
-            print(f'Job Check | {n}! is {math.factorial(n)} ')
-            found = True
-            break
+    if n > 0:
+        if n <= 12:
+            for i in range(1, math.factorial(n)):
+                if math.factorial(n) / i / (i + 1) == i + 2:
+                    print(f'Program is working | {i} * {i + 1} * {i+2} = {(i * (i + 1) * (i + 2))}')
+                    print(f'Job Check | {n}! is {math.factorial(n)} ')
+                    found = True
+                    break
+            else:
+                print('There are no such a numbers')
+        else:
+            print(f'Your number is too big | Try to enter something less than {n}')
     else:
-        print('There are no such a numbers')
+        print(f'Your number is negative or equals 0 | Try yo enter something more than {n}')
